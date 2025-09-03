4838462e1ff16614837aaa1f5f027854a565b27e
miguelmjq <mdjmirabalq@gmail.com>
2025-09-03 18:29:59 +0000

    9/3

--- Easter.java
+++ Easter.java
@@ -2,6 +2,36 @@
 // Will take a year and calculate the date that Easter falls on that year
 public class Easter {
     public static void main(String[] args){
+        double a;
+        double b;
+        double c;
+        double d;
+        double e;
+        double f;
+        double g;
+        double h;
+        double i;
+        double j;
+        double k;
+        double l;
+        double m;
+        double n;
+        double o;
+        double p;
+        double y=2026;
 
+
+        a = y%19;
+
+
+
+
+
+
+
+
+
+
+        System.out.println("a = " + a);
     }
 }
